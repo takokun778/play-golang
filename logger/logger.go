60e7f7d9c7791dfd6cb406273137d7f5dbac4662
takokun778 <takokun.778@gmail.com>
2022-02-20 21:06:34 +0900

    feat zap

---
+++ logger/logger.go
@@ -0,0 +1,32 @@
+package log
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+)
+
+var logger *zap.Logger
+
+func init() {
+	level := zap.InfoLevel
+
+	if os.Getenv("ENV") == "dev" {
+		level = zap.DebugLevel
+	}
+
+	config := zap.Config{
+		Level:            zap.NewAtomicLevelAt(level),
+		Development:      true,
+		Encoding:         "console",
+		EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	logger, _ = config.Build()
+	defer logger.Sync()
+}
+
+func Logger() *zap.SugaredLogger {
+	return logger.Sugar()
+}
